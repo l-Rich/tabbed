28964d6c4a52ee70b33adb498c35f4e7c44d5883
lauren-richardson <lauren.richardson13@gmail.com>
2022-03-09 15:46:32 -0500

    Trip database connected

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from datetime import datetime, date
+from django.contrib.auth.models import User
 
 class Trip(models.Model):
     
@@ -8,10 +9,15 @@ class Trip(models.Model):
     state = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
     date = models.DateField(auto_now_add=False, auto_now=False, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
 
 class Meta: 
     ordering = ['name']
+
+class Traveler(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,28 +2,36 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.views.generic.dates import DateDetailView
+from .models import Trip 
 
 class Home(TemplateView):
     template_name= "home.html"
 
-class Trip:
-    def __init__(self, name, country, state, city, date):
-        self.name= name
-        self.country = country
-        self.state = state
-        self.city = city
-        self.date = date
-trips = [
-    Trip("Girls Trip", "USA", "Florida", "Miami", "June 6, 2022" ), 
-    Trip("Family Vacation", "USA", "Texas", "Houston", "June 6, 2022"), 
-    Trip("Anniversary", "USA", "California", "Los Angeles", "June 6, 2022")
-]
+# class Trip:
+#     def __init__(self, name, country, state, city, date):
+#         self.name= name
+#         self.country = country
+#         self.state = state
+#         self.city = city
+#         self.date = date
+# trips = [
+#     Trip("Girls Trip", "USA", "Florida", "Miami", "June 6, 2022" ), 
+#     Trip("Family Vacation", "USA", "Texas", "Houston", "June 6, 2022"), 
+#     Trip("Anniversary", "USA", "California", "Los Angeles", "June 6, 2022")
+# ]
 
 class TripList(TemplateView):
-    template_name= "trips.html"
+    template_name= "trip_list.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["trips"] = trips 
+        name = self.request.GET.get("name")
+        if name != None:
+            context["trips"]=Trip.objects.filter(
+                name__icontains=name, user=self.request.user)
+            context["header"] = f"Searching for {name}"
+        else:
+            context["trips"] = Trip.objects.filter(user=self.request.user)
+            context["header"] = "Upcoming Trips" 
         return context
 # Create your views here.
